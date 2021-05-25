f47cfa55c585977d9b528710a315e0ae54a9bd14
fdrbrs <federio.borsci@gmail.com>
2021-05-25 17:47:30 +0200

    add more contacts

--- milestone_4/assets/js/main.js
+++ milestone_4/assets/js/main.js
@@ -89,6 +89,84 @@ const app = new Vue ({
                     }
                 ],
             },
+            {
+                name: 'Leonardo',
+                avatar: './assets/img/avatar_5.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '01/01/2021 00:30:22',
+                        text: 'Buon Anno!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '01/01/2021 00:41:33',
+                        text: 'Grazie, anche a te!',
+                        status: 'received'
+                    },
+                    {
+                        date: '01/01/2021 00:42:12',
+                        text: 'ma chi sei?',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Miriam',
+                avatar: './assets/img/avatar_6.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '02/04/2020 11:30:10',
+                        text: 'Pasta o riso?',
+                        status: 'received'
+                    },
+                    {
+                        date: '02/04/2021 12:11:13',
+                        text: 'Dipende.',
+                        status: 'sent'
+                    },
+                    {
+                        date: '01/01/2021 12:33:06',
+                        text: 'e pasta sia.',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Andrea',
+                avatar: './assets/img/avatar_7.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '03/02/2020 17:03:11',
+                        text: 'Messaggio di prova, non rispondere',
+                        status: 'sent'
+                    },
+                    {
+                        date: '03/02/2020 19:12:12',
+                        text: 'Ehi',
+                        status: 'sent'
+                    },
+                ],
+            },
+            {
+                name: 'Simone',
+                avatar: './assets/img/avatar_8.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '21/03/2020 12:10:40',
+                        text: 'Ho la barba.',
+                        status: 'received'
+                    },
+                    {
+                        date: '22/03/2020 15:31:22',
+                        text: 'Evviva.',
+                        status: 'sent'
+                    }
+                ],
+            },
         ]
     },
     methods: {
